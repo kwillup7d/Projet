83442a8cdaed79bf839a07c6fc1b979943392302
Gaidp7 <gr.lucille@hotmail.fr>
2015-12-08 18:52:35 +0100

    Create Plateau.h

---
+++ Plateau.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   Plateau.h
+ * Author: Bienvenue
+ *
+ * Created on 30 novembre 2015, 23:50
+ */
+
+#ifndef PLATEAU_H
+#define	PLATEAU_H
+#include "Case.h"
+
+class Plateau {
+public:
+    Plateau(int);
+    Plateau(const Plateau& orig);
+    virtual ~Plateau();
+    int getNombreCases();
+    Case* getCase(int);
+    virtual int lancerDe();
+    virtual bool caseLibre(Case*);
+    virtual Case* avancerPion(Case*, int);
+protected:
+    int nombrecases;
+    Case* cases;
+
+};
+
+#endif	/* PLATEAU_H */
+
