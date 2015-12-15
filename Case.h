5f810c24899b6163cee0cdeb34ad499d5377e642
Gaidp7 <gr.lucille@hotmail.fr>
2015-12-15 11:05:35 +0100

    Update Case.h

--- Case.h
+++ Case.h
@@ -8,16 +8,25 @@
 #ifndef CASE_H
 #define	CASE_H
 
+static int nbrecases = 0;
+
 class Case {
 public:
-    Case(int=0, int=0);
+    Case(int=0, int=0, int=1);
     Case(const Case& orig);
     virtual ~Case();
     int getNombrePions();
+    int getNombrePionsMax();
+    void setNombrePions(int);
     int getJouer();
+    int getNumeroCase();
     virtual int goTo();
+    bool caseEstLibre();
+    virtual int question();
 protected:
     int nombrepions;
+    int nombrepionsmax;
+    int numerocase;
     int jouer;
 };
 
