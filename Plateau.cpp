cf8cfccdb375701287a285cfac12f001e52bcb9b
Gaidp7 <gr.lucille@hotmail.fr>
2015-12-15 11:10:50 +0100

    Update Plateau.cpp

--- Plateau.cpp
+++ Plateau.cpp
@@ -6,16 +6,33 @@
  */
 
 #include "Plateau.h"
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
 
 Plateau::Plateau(int n) : nombrecases(n), cases(new Case*[n]){
+    Case* c;
     for(int i=0; i<n; i++){
-        Case c();
-        cases[i] = &c;
+        if(i%9 == 0){
+           c = new Case(0,-1,1);
+        }
+        else if(i%13 == 0)
+        {
+            c = new Case(0,1,1);
+        }
+        else {
+            c = new Case();
+        }
+        cases[i] = c;
     }
+    cout << "création du tableau" << endl;
 }
 
 Plateau::~Plateau() {
     delete [] cases;
+    cout << "destruction plateau" << endl;
 }
 
 int Plateau::getNombreCases(){
@@ -23,44 +40,57 @@ int Plateau::getNombreCases(){
 }
 
 Case* Plateau::getCase(int i){
-    return cases[i-1];
+    return cases[i];
+}
+
+void Plateau::setCase(int i, Case* c){
+    cases[i] = c;
 }
 
 int Plateau::lancerDe(){
-    return 6; //random
-}
-
-bool Plateau::caseLibre(Case* c){
-    if(c->getNombrePions()>0){
-        return false;
-    }
-    else return true;
+    srand (time(NULL));
+    return (rand()%6 + 1);
 }
 
 bool Plateau::estEnFin(Case* c){
-    return c==cases[nombrecases];
+    return c->getNumeroCase()==nombrecases-1; // On vérifie si la case est bien la dernière du plateau.
 }
 
-Case* Plateau::avancerPion(Case* c, int lancer){
-    if(!(caseLibre(c+lancer))){
-        return cases[0];
+/*Case* Plateau::avancerPion(Case* c, int lancer){
+    cout << "bouh " << endl;
+    Case* tmp = cases[0];
+    int numtmp = c->getNumeroCase() + lancer;
+    cout << c->getNumeroCase() << " c'est le plateau" << endl;
+    
+    if(numtmp > nombrecases - 1){ //Si on dépasse le nombre de cases autorisées, on recule 
+        cout << "bah" << endl;//à vérifier
+        numtmp = 2*(nombrecases -1) - (c->getNumeroCase() + lancer);
+        cout << "bah " << numtmp << endl;
+    }
+    
+    if(!(cases[numtmp]->caseEstLibre())){ // On vérifie si la case est libre, si elle ne l'est pas, on retourne au départ
+        return tmp;
     }
     else{
-        Case* tmp = cases[0];
-        if(c+lancer - cases > nombrecases){ //à vérifier
-            tmp = cases[nombrecases-1 -( c + lancer - cases - nombrecases)];
-        }
-        else{
-            tmp = c+lancer;
-        }
-        if((tmp)->goTo()!=0){
-            if(!(caseLibre(tmp+(tmp)->goTo()))){
+        cout << "bih" << endl;
+        tmp = cases[numtmp];
+        cout << tmp->goTo() << endl;
+        
+        if(tmp->goTo()>0){ // Si on peut se téléporter, on se téléporte
+            if(!(tmp+(tmp->goTo()))->caseEstLibre()){ //Si la case liée n'est pas libre, retour case départ
                 return cases[0];
             }
-            else return tmp+(tmp)->goTo();
+            else {
+                tmp = cases[tmp->getNumeroCase() + tmp->goTo()]; // Sinon on y va
+                return tmp;
+            }
+        }
+        else {
+            cout << "boh" << endl;
+            return tmp; // Sinon on va juste à la case demandée 
         }
     }
 }
-
+*/
 
 
