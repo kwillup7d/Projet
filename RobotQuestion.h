7c705054b14d62ce2d8c9edb5e54558af0a552a4
Gaidp7 <gr.lucille@hotmail.fr>
2015-12-15 11:12:27 +0100

    Create RobotQuestion.h

---
+++ RobotQuestion.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   RobotQuestion.h
+ * Author: Bienvenue
+ *
+ * Created on 14 décembre 2015, 09:55
+ */
+
+#ifndef ROBOTQUESTION_H
+#define	ROBOTQUESTION_H
+#include "JoueurQuestion.h"
+
+class RobotQuestion : public JoueurQuestion {
+public:
+    RobotQuestion(Plateau* plateau, int=1);
+    virtual ~RobotQuestion();
+    virtual int reponse();
+    virtual int choixPions();
+private:
+
+};
+
+#endif	/* ROBOTQUESTION_H */
+
