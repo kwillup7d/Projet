fd882e6bf2eb9147406da1ed058e731a1da1c3cb
Gaidp7 <gr.lucille@hotmail.fr>
2015-12-15 11:10:09 +0100

    Create JoueurQuestion.h

---
+++ JoueurQuestion.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   JoueurQuestion.h
+ * Author: Bienvenue
+ *
+ * Created on 14 décembre 2015, 09:34
+ */
+
+#ifndef JOUEURQUESTION_H
+#define	JOUEURQUESTION_H
+#include "Joueur.h"
+
+class JoueurQuestion : public Joueur {
+public:
+    JoueurQuestion(Plateau*, int=1);
+    virtual ~JoueurQuestion();
+    virtual int reponse()=0;
+    virtual Case* avancerPion(Case*, int);
+private:
+
+};
+
+#endif	/* JOUEURQUESTION_H */
+
