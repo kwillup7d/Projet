04ba358e32db610214736f46dc10ffeabf9cbbfb
Gaidp7 <gr.lucille@hotmail.fr>
2015-12-17 10:54:34 +0100

    Update Joueur.cpp

--- Joueur.cpp
+++ Joueur.cpp
@@ -7,18 +7,16 @@
 
 #include "Joueur.h"
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
-Joueur::Joueur(Plateau* pla,int n): nombrepions(n), score(0), pions(new int[n]), jouer(0), plateau(pla) {
-    for(int i=0;i<n;i++){
-        pions[i]=i+1;
-    }
+Joueur::Joueur(Plateau* pla,int n): nombrepions(n), score(0), jouer(0), plateau(pla) {
     cout << "creation joueur" << endl;
 }
 
 Joueur::~Joueur() {
-    delete [] pions;
     cout << "destruction du joueur" << endl;
 }
 
@@ -38,31 +36,37 @@ void Joueur::setJouer(int x){
     jouer = x;
 }
 
-int Joueur::getPion(int i){
-    return pions[i];
+int Joueur::choixDeplacement(){
+    srand (time(NULL));
+    return (rand()%6 + 1);
 }
 
 Case* Joueur::avancerPion(Case* c, int lancer){
     Case* tmp = plateau->getCase(0);
     int numtmp = c->getNumeroCase() + lancer;
     
-    if(numtmp > plateau->getNombreCases() - 1){ //Si on dépasse le nombre de cases autorisées, on recule 
+    //Si on dépasse le nombre de cases autorisées, on recule
+    if(numtmp > plateau->getNombreCases() - 1){  
         numtmp = 2*(plateau->getNombreCases() -1) - (c->getNumeroCase() + lancer);
     }
     
-    if((plateau->getCase(numtmp)->caseEstLibre())){// On vérifie si la case est libre, si elle ne l'est pas, on retourne au départ
+    // On vérifie si la case est libre, si elle ne l'est pas, on retourne au départ
+    if((plateau->getCase(numtmp)->caseEstLibre())){
         tmp = plateau->getCase(numtmp);
-                
-        if(tmp->goTo()>0){// Si on peut se téléporter, on se téléporte //Si la case liée n'est pas libre, retour case départ
-            if(plateau->getCase(tmp->goTo())->caseEstLibre()){
-                tmp = plateau->getCase(tmp->goTo()); // Sinon on y va
+        
+        // Si on peut se téléporter, on se téléporte 
+        if(tmp->getLienCase()>0){
+            if(plateau->getCase(tmp->getLienCase())->caseEstLibre()){
+                tmp = plateau->getCase(tmp->getLienCase());
             }
             else {
-                tmp = plateau->getCase(0); //Sinon on retourne au départ
+                //Si la case liée n'est pas libre, retour case départ
+                tmp = plateau->getCase(0); 
             }
         }
         
     }
+    //on enleve 1 car le joueur a fait son tour et on regarde si la case lui demande de rejouer ou de passer son tour
     jouer = jouer - 1 + tmp->getJouer();
     return tmp;
 }
