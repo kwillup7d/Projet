b0bc1ac40884be6a7dd11cbe30f9f29c12103028
Gaidp7 <gr.lucille@hotmail.fr>
2015-12-17 10:53:26 +0100

    Create HumainCartagena.cpp

---
+++ HumainCartagena.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   HumainCartagena.cpp
+ * Author: Bienvenue
+ * 
+ * Created on 15 décembre 2015, 21:12
+ */
+
+#include "HumainCartagena.h"
+#include <iostream>
+
+using namespace std;
+
+HumainCartagena::HumainCartagena(Plateau* plateau, int nombrecartes, int pions) : JoueurCartagena(plateau, nombrecartes, pions) {
+    cout << "creation humaincartagena" << endl;
+}
+
+HumainCartagena::~HumainCartagena() {
+    cout << "destruction humaincartagena" << endl;
+}
+
+int HumainCartagena::choixPions(){
+    int choix = 1;
+    if(nombrepions == 1){
+        return choix;
+    }
+    else{
+        cout << "Avec quel pion voulez-vous jouer ? " << endl;
+        cin >> choix;
+        return choix;
+    }
+}
+
+int HumainCartagena::choixDeplacement(){
+    char car;
+    char pioche = 'p';
+    int choix;
+    cout << "Quel symbole choisissez-vous ? (a,b,c,d,p)" << endl;
+    cin >> car;
+    while (car!=pioche || (cartes.find(car)->second) == 0){
+        cout << "Quel symbole choisissez-vous ? (a,b,c,d,p)" << endl;
+        cin >> car;
+    }
+    choix = (int)car;
+    return choix;
+}
+
