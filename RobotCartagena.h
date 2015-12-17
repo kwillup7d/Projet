c9810903c5bd9bbe8b91511cbb3106551eec7f2d
Gaidp7 <gr.lucille@hotmail.fr>
2015-12-17 10:52:18 +0100

    Create RobotCartagena.h

---
+++ RobotCartagena.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   RobotCartagena.h
+ * Author: Bienvenue
+ *
+ * Created on 15 décembre 2015, 21:17
+ */
+
+#ifndef ROBOTCARTAGENA_H
+#define	ROBOTCARTAGENA_H
+#include "JoueurCartagena.h"
+
+class RobotCartagena : public JoueurCartagena{
+public:
+    RobotCartagena(Plateau*,int,int=1);
+    virtual ~RobotCartagena();
+    virtual int choixPions();
+    virtual int choixDeplacement();
+private:
+
+};
+
+#endif	/* ROBOTCARTAGENA_H */
+
